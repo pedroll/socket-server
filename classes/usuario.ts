b828aad59281f804d516e41069e28b0590b3ef07
pedro <pedrollongo@gmail.com>
2019-04-08 20:19:36 +0200

    40 Manejando usuarios conectados en el socket-server

---
+++ classes/usuario.ts
@@ -0,0 +1,12 @@
+export class Usuario {
+    id: string;
+    nombre: string;
+    sala: string;
+
+    constructor(id: string) {
+        this.id = id;
+        this.nombre = 'sin-nombre';
+        this.sala = 'sin-sala';
+
+    }
+}
---
+++ classes/usuarios-lista.ts
@@ -0,0 +1,50 @@
+import Debug from 'debug';
+import * as environment from '../global/enviroment';
+import {Usuario} from './usuario';
+
+const debug = Debug(environment.DEBUG);
+
+export class UsuariosLista {
+
+    private lista: Array<Usuario> = [];
+
+    agregarUsuario(usuario: Usuario): Usuario {
+        this.lista.push(usuario);
+        debug('lista', this.lista);
+
+        return usuario;
+    }
+
+    actualizarNombre(id: string, nombre: string): void {
+
+        for (const usuario of this.lista) {
+            if (usuario.id === id) {
+                usuario.nombre = nombre;
+                break;
+            }
+        }
+
+        debug('======== Actualizando usuario ===========');
+        debug(this.lista);
+    }
+
+    getLista(): Array<Usuario> {
+        return this.lista;
+    }
+
+    getUsuario(id: string): Usuario | void {
+        return this.lista.find(usuario => usuario.id === id);
+    }
+
+    getUsuariosSala(sala: string): Array<Usuario> | void {
+        return this.lista.filter(usuario => usuario.sala === sala);
+    }
+
+    borrarUsuario(id: string): void | Usuario {
+        const tempUsuario = this.getUsuario(id);
+        this.lista = this.lista.filter(usuario => usuario !== tempUsuario);
+        debug('Usuario borrado', tempUsuario);
+
+        return tempUsuario;
+    }
+}
